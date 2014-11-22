a0e23855e2a49312acdeb5970b68f1d9aef8819d
AlbertYang <yzt0806@gmail.com>
2014-11-22 14:31:03 +0800

    import the pinyin

---
+++ src/demo/Demo.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class Demo {
+	public static void main(String[] args) throws Exception {
+		Hz2Py hp = new Hz2Py();
+		String strPinyin = hp.getStringPinYin("你好king国庆");
+		System.out.println(strPinyin);
+	}
+}
